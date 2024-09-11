0e4328669441b44e103118428662bf0f431e5eba
Samson Mesioye <jazzygee36gmail.com>
2024-09-11 22:30:49 +0100

    dashboard

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import NavigationDashboard from '../../components/navigationDashboard/navigationDashboard';
 
 const Dashboard = () => {
-  return (
-    <NavigationDashboard title='Dashboard'>
-      <div>board</div>
-    </NavigationDashboard>
-  );
+  return <NavigationDashboard title='Dashboard'>board</NavigationDashboard>;
 };
 
 export default Dashboard;
